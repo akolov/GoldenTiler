5c18357148571fdc0bc527d1a1f56bc94c2a333e
Alexander Kolov <me@alexkolov.com>
2015-10-05 02:06:51 +0200

    Move computation to separate method

--- GoldenTiler/ViewController.swift
+++ GoldenTiler/ViewController.swift
@@ -64,34 +64,12 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
   // MARK: UIImagePickerControllerDelegate
 
   func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-    let mediaType = info[UIImagePickerControllerMediaType] as? String
-
-    guard mediaType == String(kUTTypeImage) else {
-      return
+    dismissViewControllerAnimated(true) { [weak self] in
+      self?.applyMediaFiltersAndDisplay(info)
     }
+  }
 
-    var image: CIImage?
-
-    if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
-      image = CIImage(image: editedImage)
-    }
-    else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-      image = CIImage(image: originalImage)
-    }
-
-    if let image = image {
-      let filter = GoldenSpiralFilter()
-      filter.inputImage = image
-      imageView.image = filter.outputImage
-    }
-
-    if let image = imageView.image {
-      scrollView.minimumZoomScale = minimumImageZoomScale
-      scrollView.zoomScale = scrollView.minimumZoomScale
-      imageViewWidthConstraint.constant = image.extent.width
-      imageViewHeightConstraint.constant = image.extent.height
-    }
-
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
     dismissViewControllerAnimated(true, completion: nil)
   }
 
@@ -114,6 +92,43 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
 
     return scrollView.bounds.width / image.extent.width
+  }
+
+  private func applyMediaFiltersAndDisplay(media: [String : AnyObject]) {
+    let mediaType = media[UIImagePickerControllerMediaType] as? String
+
+    guard mediaType == String(kUTTypeImage) else {
+      return
+    }
+
+    var image: CIImage?
+
+    if let editedImage = media[UIImagePickerControllerEditedImage] as? UIImage {
+      image = CIImage(image: editedImage)
+    }
+    else if let originalImage = media[UIImagePickerControllerOriginalImage] as? UIImage {
+      image = CIImage(image: originalImage)
+    }
+
+    if let image = image {
+      let filter = GoldenSpiralFilter()
+
+      if image.extent.height > image.extent.width {
+        filter.inputImage = image.imageByApplyingOrientation(8)
+        imageView.image = filter.outputImage?.imageByApplyingOrientation(7)
+      }
+      else {
+        filter.inputImage = image.imageByApplyingOrientation(4)
+        imageView.image = filter.outputImage
+      }
+    }
+
+    if let image = imageView.image {
+      scrollView.minimumZoomScale = minimumImageZoomScale
+      scrollView.zoomScale = scrollView.minimumZoomScale
+      imageViewWidthConstraint.constant = image.extent.width
+      imageViewHeightConstraint.constant = image.extent.height
+    }
   }
 
 }
